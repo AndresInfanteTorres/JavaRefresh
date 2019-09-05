02d11f0d4c1fe47361f6fd4fc95fee5241b4ef35
anubis <anubis@DESKTOP-IHIN9AC>
2019-09-04 23:38:13 -0500

    Initial commit from eclipse.

---
+++ javaRefresh/src/testAndres.java
@@ -0,0 +1,128 @@
+
+import java.util.Arrays;
+
+class testAndres {
+
+	private static String describirArreglo(int[] arr) {
+
+		boolean asc = true, des = true;
+		int band1 = 1, band2 = 0, band3 = 0;
+
+		for (int i = 0; i + 1 < arr.length; i++) {
+			if (arr[i] < arr[i + 1]) {
+				des = false;
+				band2 = 1;
+			}
+			if (arr[i] > arr[i + 1]) {
+				asc = false;
+				band3 = 1;
+			}
+			if (arr[i] == arr[i + 1]) {
+				band1 += 1;
+			}
+		}
+
+		if (asc & (des == false)) {
+
+			return "ascendiente";
+		}
+		if (des & (asc == false)) {
+			return "descendiente";
+		}
+		if (band1 == arr.length) {
+			return "Todos son iguales";
+		}
+		if ((band2 & band3) == 1) {
+			return "Desordenados";
+		}
+
+		return null;
+	}
+
+	private static void ordenarArreglo(int[] arr) {
+
+		for (int i = 0; i < arr.length - 1; ++i) {
+			for (int j = i + 1; j < arr.length; ++j) {
+				if (arr[i] < arr[j]) {
+					int aux = arr[i];
+					arr[i] = arr[j];
+					arr[j] = aux;
+				}
+			}
+		}
+
+	}
+
+	private static String palindromo(String word) {
+		int size = (word.length()) / 2;
+		char[] wordarray = word.toCharArray();
+		int band = 0;
+		int j = word.length() - 1;
+		for (int i = 0; i < size; i++) {
+			if (wordarray[i] != wordarray[j]) {
+				return "no palindromo";
+			}
+			j -= 1;
+			band += 1;
+		}
+		if (band == size) {
+			return "palindromo";
+		}
+		return null;
+	}
+	
+	private static String tipoCadena(String cadena) {
+		
+		char[] arreglo=cadena.toCharArray();
+		
+		
+		
+		return null;
+	}
+	private static boolean numero(char x) {
+		String numeros ="0123456789";
+		char [] arrNumeros= numeros.toCharArray();
+		for (int i=0;i<10;i++) {
+			if (x==arrNumeros[1]) {
+				return true;
+			}
+			break;
+		}
+		return false;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("andy");
+		int[] arr = { 1, 2, 2, 2, 2, 22, 4, 10, 4, 22, 22, 22, 3 };
+		System.out.println(describirArreglo(arr));
+
+		int[] arr1 = { 3, 2, 1 };
+		System.out.println(describirArreglo(arr1));
+
+		int[] arr2 = { 1, 1, 1, 1 };
+		System.out.println(describirArreglo(arr2));
+
+		int[] arr3 = { 1, 2, 3, 1 };
+		System.out.println(describirArreglo(arr3));
+
+		// lamdo al metodo ordenar arreglo
+
+		System.out.println(Arrays.toString(arr));
+
+		// llamada a la funcion que determina si es un palindromo
+
+		System.out.println(palindromo("alle"));
+		System.out.println(palindromo("alla"));
+		System.out.println(palindromo("allas"));
+		System.out.println(palindromo("alsla"));
+		System.out.println(palindromo("amor a roma"));
+		
+		System.out.println("validando funcion numero");
+		System.out.println(numero('3'));
+
+	}
+
+}
